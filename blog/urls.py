927b21094cde38557547cf20117bde02466ecf06
emilianososa93 <emiliano_sosa93@hotmail.com>
2018-09-01 12:50:28 -0300

    login etapa3

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
         url(r'^$', views.post_list),
-        url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+        url(r'^post/(?P<pk>[0-9]+)/detail/$', views.post_detail, name='post_detail'),
         url(r'^post/new/$', views.post_new, name='post_new'),
         url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
         url(r'^post/login/$', views.post_login, name ='post_login'),
