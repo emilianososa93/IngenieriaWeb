aaf9afe49cd182b787d749f18e0fffc908c6ee26
emilianososa93 <emiliano_sosa93@hotmail.com>
2018-09-12 20:04:58 -0300

    actualizacion sitio

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import login
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-		url(r'^$', views.post_list, name='post_list'),
+		url(r'^$', views.post_portada, name='post_portada'),
 		url(r'^post/$', views.post_list, name='post_list'),
         url(r'^post/(?P<pk>[0-9]+)/detail/$', views.post_detail, name='post_detail'),
         url(r'^post/new/$', views.post_new, name='post_new'),
