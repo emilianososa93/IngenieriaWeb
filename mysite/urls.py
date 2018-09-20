181b4e6f8bc5f2e4eec08f1dec66614ea5917766
emilianososa93 <emiliano_sosa93@hotmail.com>
2018-09-20 00:24:49 -0300

    url

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,8 @@ from django.contrib.auth.views import login,logout_then_login
 urlpatterns = [
 	url(r'^$', views.post_portada, name='post_portada'),
 	url(r'', include('blog.urls')),
-	url(r'^post/account(?P<activacion_token>)/', views.post_confirmar, name ='post_portada'),
+	url(r'^post/portada/', views.post_portada, name ='post_portada'),
+	#url(r'^post/portada(?P<activacion_token>)/', views.post_confirmar, name ='post_portada'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', views.post_login, name ='post_login' ),
     url(r'^post/logout/$', logout_then_login, name ='post_logout' ),
