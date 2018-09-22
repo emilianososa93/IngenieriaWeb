fb48f49de128dae25f6e6ef34d5b60ea8f01e845
emilianososa93 <emiliano_sosa93@hotmail.com>
2018-09-22 20:12:19 -0300

    secciones agregadas

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'America/Argentina/Buenos_Aires'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
